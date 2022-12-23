9ce81ef6f5df1244b3331b2a013b7ef85576299f
Jason <pan.jason12@gmail.com>
2022-12-23 16:16:01 -0500

    added gc selection bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,25 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
+import { ChannelListContainer, ChannelContainer } from './components';
+
+import './App.css';
+
 const apiKey = 'kkkhqz2qtsx6';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-        <h1>Waste My Time</h1>
+    <div className="app__wrapper">
+        <Chat client={client} theme="team light">
+            <ChannelListContainer
+
+            />
+            <ChannelContainer
+
+            />
+        </Chat>
     </div>
   );
 }
