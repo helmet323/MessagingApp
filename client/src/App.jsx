38aa284858a77f795d3b95ac687e063166ca8792
Rui <rui.li.student@gmail.com>
2022-12-24 13:49:47 -0500

    backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,26 @@ import Cookies from 'universal-cookie';
 import { ChannelListContainer, ChannelContainer, Auth } from './components';
 import './App.css';
 
+const cookies = new Cookies();
+
 const apiKey = 'kkkhqz2qtsx6';
+const authToken = cookies.get("token");;
+
 const client = StreamChat.getInstance(apiKey);
-const authToken = true;
+
+if (authToken){
+  client.connectUser({
+      id: cookies.get('userId'),
+      name: cookies.get('username'),
+      fullName: cookies.get('fullName'),
+      image: cookies.get('avatarURL'),
+      hashedPassword: cookies.get('hashedPassword'),
+      phoneNumber: cookies.get('phoneNumber'),
+  }, authToken)
+}
 
 const App = () => {
+
   if(!authToken) return <Auth />;
 
   return (
